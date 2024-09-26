3c0958ac4052a1d1b048662fcb360f7e9577f0e2
karnhao <srv33605@gmail.com>
2024-09-26 19:09:07 +0700

    MySQL Statement Test

--- server/sa-server/src/main/java/ku/cs/Main.java
+++ server/sa-server/src/main/java/ku/cs/Main.java
@@ -2,15 +2,34 @@ package ku.cs;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import com.sun.net.httpserver.HttpServer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
+        // Connect to Database
+        Connection conn = null;
+        String dataSourceUrl = "jdbc:mysql://localhost/sa";
+        String user = "root";
+        String password = "";
+
+        try {
+            conn = DriverManager.getConnection(dataSourceUrl, user, password);
+        } catch (SQLException e) {
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+        }
+
+
         int port = 25565;
 
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-        server.createContext("/", new MyHandler());
+        server.createContext("/", new MyHandler(conn));
 
         server.setExecutor(null);
         server.start();
--- server/sa-server/src/main/java/ku/cs/MyHandler.java
+++ server/sa-server/src/main/java/ku/cs/MyHandler.java
@@ -2,15 +2,55 @@ package ku.cs;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 public class MyHandler implements HttpHandler {
 
+    private Connection connection;
+    private Statement statement;
+    private ResultSet resultSet;
+
+    public MyHandler(Connection connection) {
+        this.connection = connection;
+        this.statement = null;
+        this.resultSet = null;
+    }
+
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        String response = "Hello World";
+        String response = "NO DATA";
+        int columnCount;
+        try {
+            this.statement = connection.createStatement();
+            this.resultSet = this.statement.executeQuery("SELECT * FROM USER");
+            response = "Row : " + this.resultSet.getRow() + "\n" + this.resultSet.getMetaData() + "\n";
+
+            columnCount = resultSet.getMetaData().getColumnCount();
+            while (this.resultSet.next()) {
+                for (int i = 1; i <= columnCount; i++) {
+                    response += this.resultSet.getObject(i).toString() + ", ";
+                }
+                response += "\n";
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                this.statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        
+
         exchange.sendResponseHeaders(200, response.length());
         OutputStream os = exchange.getResponseBody();
         os.write(response.getBytes());
