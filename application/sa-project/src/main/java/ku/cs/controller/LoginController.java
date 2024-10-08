4b534ae70045549e0f69cf27833159cdbdb98122
karnhao <srv33605@gmail.com>
2024-10-08 16:15:54 +0700

    login front-end

--- application/sa-project/src/main/java/ku/cs/controller/LoginController.java
+++ application/sa-project/src/main/java/ku/cs/controller/LoginController.java
@@ -1,11 +1,15 @@
 package ku.cs.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import ku.cs.service.RootService;
 
 
 public class LoginController {
-    
+
+    public TextField passwordField;
+    public TextField usernameField;
+
     @FXML
     public void initialize() {
 
@@ -13,5 +17,13 @@ public class LoginController {
 
     public void onSignUpButton() {
         RootService.getController().open("register.fxml");
+    }
+
+    public void onLogin() {
+        RootService.getController().open("register.fxml");
+    }
+
+    public void onForgotPassword() {
+
     }
 }
