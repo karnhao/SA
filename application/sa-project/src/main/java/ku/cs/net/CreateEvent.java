b7ca54eec773d18fa4daa7c00cdef63b3fe6be08
karnhao <srv33605@gmail.com>
2024-10-24 19:21:29 +0700

    fix error

--- application/sa-project/src/main/java/ku/cs/net/CreateEvent.java
+++ application/sa-project/src/main/java/ku/cs/net/CreateEvent.java
@@ -8,7 +8,7 @@ import java.net.URISyntaxException;
 import java.time.LocalDateTime;
 
 public class CreateEvent {
-    public void createEvent(String eventName, LocalDateTime startDateTime, LocalDateTime endDateTime, String description) throws URISyntaxException, IOException {
+    public String createEvent(String eventName, LocalDateTime startDateTime, LocalDateTime endDateTime, String description) throws URISyntaxException, IOException {
         JSONObject jsonObject = new JSONObject();
 
         HttpURLConnection httpURLConnection = Client.getClient().getHttpURLConnection(jsonObject.toString(), "/create_event");
