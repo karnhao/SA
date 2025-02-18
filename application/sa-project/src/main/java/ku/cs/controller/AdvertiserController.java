3e6e7f6188d2520d84ef06e445fee76936a30163
amonnuts <amonnut.s@ku.th>
2025-02-18 23:54:07 +0700

    add advertiser frame

---
+++ application/sa-project/src/main/java/ku/cs/controller/AdvertiserController.java
@@ -0,0 +1,11 @@
+package ku.cs.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+
+public class AdvertiserController {
+
+    @FXML private ImageView advertiserImage;
+
+}
