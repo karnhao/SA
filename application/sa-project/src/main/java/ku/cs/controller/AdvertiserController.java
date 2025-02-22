57ecb9dfbb8503ec59c3643d4428090c2e497be0
amonnuts <amonnut.s@ku.th>
2025-02-22 19:18:28 +0700

    add code/not finish

--- application/sa-project/src/main/java/ku/cs/controller/AdvertiserController.java
+++ application/sa-project/src/main/java/ku/cs/controller/AdvertiserController.java
@@ -1,11 +1,25 @@
 package ku.cs.controller;
 
+import java.net.URL;
+
+import javafx.animation.Animation;
+import javafx.animation.Interpolator;
+import javafx.animation.RotateTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.transform.Rotate;
+import javafx.util.Duration;
 
 public class AdvertiserController {
 
     @FXML private ImageView advertiserImage;
-
+    @FXML
+    public void initialize() {
+        URL url = getClass().getResource("/ku/cs/images/advertiser/heimer.png");
+        if (url != null) {
+            advertiserImage.setImage(new Image(url.toExternalForm()));
+        }
+    }
 }
